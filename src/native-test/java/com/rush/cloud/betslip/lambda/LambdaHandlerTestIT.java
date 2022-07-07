8d646d084ddfb17a8c8ae8e3a58def5c8aa3adbf
Ian Ong <ong@rushstreetinteractive.com>
2022-07-07 12:11:08 -0600

    Initial commit

---
+++ src/native-test/java/com/rush/cloud/betslip/lambda/LambdaHandlerTestIT.java
@@ -0,0 +1,9 @@
+package com.rush.cloud.betslip.lambda;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class LambdaHandlerTestIT extends LambdaHandlerTest {
+
+    // Execute the same tests but in native mode.
+}
